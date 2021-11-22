edf9578bd3bf305aef459cfa922ef9a7f1ffeab6
Adam <adamlenter1@gmail.com>
2021-11-21 19:49:32 -0500

    Created Overall Scoreboard Screen.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 let familyMembers = [];
 let familyMemberIDs = [];
 let dailyLogs = [];
+let totalFamilyMemberScore = [];
+let totalFamilyMemberAnnualScore = [];
+let totalFamilyMemberMonthlyScore = [];
+let startYear = 100000;
+let endYear = 0;
 
 function calculateDailyScore(dailyLog) {
     const dailyPoints = dailyLog.foodBalance + dailyLog.foodQuantity + dailyLog.exercise;
@@ -37,24 +42,45 @@ fetch(`http://localhost:3000/dailyLogs`)
     .then((result) => result.json()) 
     .then((allLogs) => {
         for(let i = 0; i < allLogs.length; i++) {
+            const score = calculateDailyScore(allLogs[i]);
+            
             if(dailyLogs[allLogs[i].familyMemberID] === undefined) {
+                //The family member does not have a list of logs yet:
                 dailyLogs[allLogs[i].familyMemberID] = [];
+                totalFamilyMemberScore[allLogs[i].familyMemberID] = 0;
+
+                //Create an annual and monthly score array:
+                totalFamilyMemberAnnualScore[allLogs[i].familyMemberID] = [];
+                totalFamilyMemberMonthlyScore[allLogs[i].familyMemberID] = [];
             }
 
             if(dailyLogs[allLogs[i].familyMemberID][allLogs[i].year] === undefined) {
+                //The family member does not have a list of logs for the given year. Create one.
                 dailyLogs[allLogs[i].familyMemberID][allLogs[i].year] = [];
+
+                //Create an annual total for the year:
+                totalFamilyMemberAnnualScore[allLogs[i].familyMemberID][allLogs[i].year] = 0;
+
+                //Update the starting and ending years:
+                startYear = Math.min(startYear, allLogs[i].year)
+                endYear = Math.max(endYear, allLogs[i].year)
             }
             
             if(dailyLogs[allLogs[i].familyMemberID][allLogs[i].year][allLogs[i].month] === undefined) {
                 dailyLogs[allLogs[i].familyMemberID][allLogs[i].year][allLogs[i].month] = [];
             }
 
+            //Now add the log to the appropriate array:
             dailyLogs[allLogs[i].familyMemberID][allLogs[i].year][allLogs[i].month][allLogs[i].dateNumber] = {
                 foodBalance: allLogs[i].foodBalance, 
                 foodQuantity: allLogs[i].foodQuantity, 
                 exercise: allLogs[i].exercise, 
-                score: calculateDailyScore(allLogs[i])
+                score: score
             };
+
+            //Add the score to all of the appropriate totals:
+            totalFamilyMemberScore[allLogs[i].familyMemberID] += score;
+            totalFamilyMemberAnnualScore[allLogs[i].familyMemberID][allLogs[i].year] += score;
         }
     })
 
@@ -93,12 +119,23 @@ function createTableHeadingCell(tableRow, headingText) {
     return headingCell;
 }
 
-function createTableDataCell(tableRow, cellText) {
+function createNameHeadings(tableHeadingRow) {
+    for(let i = 0; i < familyMemberIDs.length; i++) {
+        const cell = createTableHeadingCell(tableHeadingRow, familyMembers[familyMemberIDs[i]].firstName + " " + familyMembers[familyMemberIDs[i]].lastName)
+    }
+    return;
+}
+
+function createTableDataCell(tableRow, cellText, bold1) {
     const dataCell = document.createElement("td");
     dataCell.textContent = cellText;
     tableRow.appendChild(dataCell);
+
+    if(bold1 === 1) {
+        dataCell.style.fontWeight = 'bold';
+    }
     return dataCell;
-}
+}  
 
 function createForm(div) {
     const form = document.createElement("form");
@@ -137,6 +174,13 @@ function createButtonDivWithButton(form, submitButtonText) {
     buttonDiv.appendChild(button);
 
     return button;
+}
+
+function createLineBreaks(elementToAppendTo, numberOfLineBreaks) {
+    for(let i = 0; i <= numberOfLineBreaks; i++) {
+        const linebreak = document.createElement("br");
+        elementToAppendTo.appendChild(linebreak);
+    }
 }
 
 function createCenteredLink(elementToAppend, linkText) {
@@ -179,6 +223,8 @@ function showFamilyMembers() {
             displayEnterEditFamilyMemberForm(event.target.id);
         })
     }
+
+    createLineBreaks(screenDiv, 1);
 
     let link = createCenteredLink(screenDiv, "Create new family member");
     link.addEventListener("click", (event) => {
@@ -283,6 +329,49 @@ function displayEnterEditFamilyMemberForm(familyMemberID) {
     }
 }
 
+function showOverallScoreboard() {
+    const screenDiv = createScreen("Lifetime Scoreboard");
+    const table = createTable(screenDiv, "Scoreboard");
+    const headingRow = createTableRow(table);
+
+    const yearHeading = createTableHeadingCell(headingRow, "Year");
+    createNameHeadings(headingRow);
+
+    //Now create rows by year:
+    for(let year = startYear; year <= endYear; year++) {
+        const yearRow = createTableRow(table);
+
+        //Display the year in a cell:
+        const yearCell = createTableDataCell(yearRow, year);
+
+        for(let i = 0; i < familyMemberIDs.length; i++) {
+            if(totalFamilyMemberAnnualScore[familyMemberIDs[i]] && totalFamilyMemberAnnualScore[familyMemberIDs[i]][year]) {
+                //There is a value for the family member and year:
+                const cell = createTableDataCell(yearRow, totalFamilyMemberAnnualScore[familyMemberIDs[i]][year]);
+            }
+            else {
+                //There is no value:
+                const cell = createTableDataCell(yearRow, "-");
+            }
+        }
+    }
+
+    const totalRow = createTableRow(table)
+
+    const totalCell = createTableDataCell(totalRow, "TOTAL: ", 1);
+
+    for(let i = 0; i < familyMemberIDs.length; i++) {
+        if(totalFamilyMemberScore[familyMemberIDs[i]]) {
+            //There is a value for the family member and year:
+            const cell = createTableDataCell(totalRow, totalFamilyMemberScore[familyMemberIDs[i]], 1);
+        }
+        else {
+            //There is no value:
+            const cell = createTableDataCell(totalRow, "-", 1);
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("menu_icon").addEventListener("click", (event) => {
         if(document.getElementById("navigationMenu").style.display === "block") {
@@ -302,5 +391,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("menuFamilyMembers").addEventListener("click", (event) => {
         document.getElementById("navigationMenu").style.display = "none";
         showFamilyMembers()
+    })
+
+    document.getElementById("menuOverallScoreboard").addEventListener("click", (event) => {
+        document.getElementById("navigationMenu").style.display = "none";
+        showOverallScoreboard()
     })
 })
