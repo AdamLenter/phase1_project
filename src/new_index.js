565b6cc5625843f4f948ead729d9ca696b43f7a8
Adam <adamlenter1@gmail.com>
2021-11-24 12:25:53 -0500

    Moved menu funcationality into the .then after content is retrieved. That way, until data is retrived, the menu doesn't work.

--- src/new_index.js
+++ src/new_index.js
@@ -2,43 +2,41 @@ let familyMembers;
 let dailyLogs;
 
 
-const fetchFamily = () => {
+function fetchFamily() {
     fetch(`http://localhost:3000/familyMembers`) 
        .then((result) => result.json()) 
         .then((allFamilyMembers) => {
             familyMembers = allFamilyMembers;
+            document.getElementById("menu_icon").addEventListener("click", (event) => {
+                if(document.getElementById("navigationMenu").style.display === "block") {
+                    //The menu is currently displayed. Hide it:
+                    document.getElementById("navigationMenu").style.display = "none"
+                }
+                else {
+                    //The menu is not displayed. Show it:
+                    document.getElementById("navigationMenu").style.display = "block";
+                }
+            })
+        
+            document.getElementById("navigationMenu").addEventListener("mouseleave", (event) => {
+                document.getElementById("navigationMenu").style.display = "none";
+            })
+        
+            document.getElementById("menuFamilyMembers").addEventListener("click", (event) => {
+                document.getElementById("navigationMenu").style.display = "none";
+                showFamilyMembers()
+            })
+        
+            document.getElementById("menuOverallScoreboard").addEventListener("click", (event) => {
+                document.getElementById("navigationMenu").style.display = "none";
+                showOverallScoreboard()
+            })
         })
-}
-
+    }
 
 //Menu Functionality:
 document.addEventListener("DOMContentLoaded", () => {
     fetchFamily();
-
-    document.getElementById("menu_icon").addEventListener("click", (event) => {
-        if(document.getElementById("navigationMenu").style.display === "block") {
-            //The menu is currently displayed. Hide it:
-            document.getElementById("navigationMenu").style.display = "none"
-        }
-        else {
-            //The menu is not displayed. Show it:
-            document.getElementById("navigationMenu").style.display = "block";
-        }
-    })
-
-    document.getElementById("navigationMenu").addEventListener("mouseleave", (event) => {
-        document.getElementById("navigationMenu").style.display = "none";
-    })
-
-    document.getElementById("menuFamilyMembers").addEventListener("click", (event) => {
-        document.getElementById("navigationMenu").style.display = "none";
-        showFamilyMembers()
-    })
-
-    document.getElementById("menuOverallScoreboard").addEventListener("click", (event) => {
-        document.getElementById("navigationMenu").style.display = "none";
-        showOverallScoreboard()
-    })
 })
 
 //Screen to see lifetime performance
